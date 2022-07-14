de50f67a0b62d9fa6f32f48176e0ac0c7eaf023a
Axen <65947351+1Axen@users.noreply.github.com>
2022-07-14 10:04:06 +0300

    Add files via upload

---
+++ SettingsLibrary.lua
@@ -0,0 +1,42 @@
+local Settings = {}
+local HttpService = game:GetService("HttpService")
+
+function Settings.new(Name)
+    local Items = {}
+    local Object = setmetatable({}, {
+        __iter = function(self)
+            return next, Items
+        end,
+
+        __index = function(self, key)
+            return Items[key]
+        end,
+
+        __newindex = function(self, key, value)
+            Items[key] = value 
+        end
+    })
+    
+
+    function Object.Add(Directory, Values)
+        Object._items[Directory] = Values
+    end
+
+    function Object.Save()
+        writefile(Name, HttpService:JSONEncode(Items))
+    end
+
+    function Object.Load()
+        local Success, Content = pcall(function()
+            readfile(Name)
+        end)
+
+        if Success then
+            Items = HttpService:JSONDecode(Content)
+        end
+    end
+
+    return Object
+end
+
+return Settings
